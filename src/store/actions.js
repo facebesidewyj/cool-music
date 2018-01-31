cee30247f16da5044ca1500d233d87286c833002
facebesidewyj <wang.yijin@foxmail.com>
2018-01-31 23:10:25 +0800

    update vuex state

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,8 +5,8 @@ import * as types from './mutation-types';
 
 export function selectPlay({ commit, state }, { list, index }) {
   commit(types.SET_SEQUENCE_LIST, list);
-  commit(types.SET_FULL_SCREEN, state.fullScreen);
+  commit(types.SET_FULL_SCREEN, true);
   commit(types.SET_PLAY_LIST, list);
-  commit(types.SET_PLAY_STATE, state.playState);
+  commit(types.SET_PLAY_STATE, true);
   commit(types.SET_CURRENT_INDEX, index);
 }
