0c18964d4462e2926ad8ca669f114a5233b0197a
facebesidewyj <wang.yijin@foxmail.com>
2018-02-01 23:18:25 +0800

    update player component

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,67 +1,72 @@
 <template lang="html">
 <div class="player-wrapper" v-if="playList.length > 0">
-  <div class="full-screen-player" v-show="fullScreen">
-    <div class="background">
-      <img :src="currentSong.image" alt="背景图" width="100%" height="100%">
-    </div>
-    <!-- 顶部布局 -->
-    <div class="top">
-      <div class="icon-back-wrapper" @click="togglePlayer">
-        <i class="icon-back"></i>
+  <transition name="normal">
+    <div class="full-screen-player" v-show="fullScreen">
+      <div class="background">
+        <img :src="currentSong.image" alt="背景图" width="100%" height="100%">
       </div>
-      <h1 class="song-name">{{currentSong.album}}</h1>
-      <h2 class="singer-name" v-html="currentSong.singer"></h2>
-    </div>
-    <!-- 中间布局 -->
-    <div class="middle">
-      <div class="cd">
-        <img :src="currentSong.image" alt="cd">
+      <!-- 顶部布局 -->
+      <div class="top">
+        <div class="icon-back-wrapper" @click="togglePlayer">
+          <i class="icon-back"></i>
+        </div>
+        <h1 class="song-name">{{currentSong.album}}</h1>
+        <h2 class="singer-name" v-html="currentSong.singer"></h2>
       </div>
-      <div class="song-desc-wrapper">
-        <div class="song-desc">{{songDesc}}</div>
-      </div>
-    </div>
-    <!-- 底部布局 -->
-    <div class="bottom">
-      <div class="operators-wrapper">
-        <div class="icon-wrapper">
-          <i class="icon-sequence"></i>
+      <!-- 中间布局 -->
+      <div class="middle">
+        <div class="cd" :class="cdRotate">
+          <img :src="currentSong.image" alt="cd">
         </div>
-        <div class="icon-wrapper">
-          <i class="icon-prev"></i>
-        </div>
-        <div class="icon-wrapper">
-          <i class="icon-play"></i>
-        </div>
-        <div class="icon-wrapper">
-          <i class="icon-next"></i>
-        </div>
-        <div class="icon-wrapper">
-          <i class="icon-not-favorite"></i>
+        <div class="song-desc-wrapper">
+          <div class="song-desc">{{songDesc}}</div>
         </div>
       </div>
-    </div>
-  </div>
-  <div class="mini-player" v-show="!fullScreen">
-    <div class="mini-cd">
-      <img :src="currentSong.image" alt="cd">
-    </div>
-    <div class="mini-content-wrapper">
-      <h1 class="mini-song-name">{{currentSong.name}}</h1>
-      <p class="mini-singer-name">{{currentSong.singer}}</p>
-    </div>
-    <div class="mini-operator-wrapper">
-      <div class="mini-icon-wrapper">
-        <i class="icon-play"></i>
+      <!-- 底部布局 -->
+      <div class="bottom">
+        <div class="operators-wrapper">
+          <div class="icon-wrapper">
+            <i class="icon-sequence"></i>
+          </div>
+          <div class="icon-wrapper">
+            <i class="icon-prev"></i>
+          </div>
+          <div class="icon-wrapper" @click="togglePlayState">
+            <i :class="playIcon"></i>
+          </div>
+          <div class="icon-wrapper">
+            <i class="icon-next"></i>
+          </div>
+          <div class="icon-wrapper">
+            <i class="icon-not-favorite"></i>
+          </div>
+        </div>
       </div>
-
     </div>
-  </div>
+  </transition>
+  <transition name="mini">
+    <div class="mini-player" v-show="!fullScreen" @click="togglePlayer">
+      <div class="mini-cd">
+        <img :src="currentSong.image" alt="cd" :class="cdRotate">
+      </div>
+      <div class="mini-content-wrapper">
+        <h1 class="mini-song-name">{{currentSong.name}}</h1>
+        <p class="mini-singer-name">{{currentSong.singer}}</p>
+      </div>
+      <div class="mini-operator-wrapper">
+        <div class="mini-icon-wrapper" @click.stop="togglePlayState">
+          <i :class="miniPlayIcon"></i>
+        </div>
+      </div>
+    </div>
+  </transition>
+  <audio :src="currentSong.url" ref="audio"></audio>
 </div>
 </template>
 
 <script type="text/ecmascript-6">
-import { mapGetters } from 'vuex';
+// 引入vuex的语法糖
+import { mapGetters, mapMutations } from 'vuex';
 
 export default {
   name: 'player',
@@ -69,14 +74,76 @@ export default {
     return {};
   },
   computed: {
+    /**
+     * 控制cd旋转动画
+     */
+    cdRotate() {
+      return this.playState ? 'play' : 'play pause';
+    },
+
     songDesc() {
       return this.currentSong.album + '-' + this.currentSong.singer;
     },
-    ...mapGetters(['playList', 'fullScreen', 'currentSong'])
+
+    /**
+     * 全屏播放器播放按钮展示
+     */
+    playIcon() {
+      if (this.playState) {
+        return 'icon-pause';
+      } else {
+        return 'icon-play';
+      }
+    },
+
+    /**
+     * 迷你播放器播放按钮展示
+     */
+    miniPlayIcon() {
+      return this.playState ? 'icon-pause-mini' : 'icon-play-mini';
+    },
+    ...mapGetters(['playList', 'fullScreen', 'currentSong', 'playState'])
   },
   methods: {
+    /**
+     * 点击切换播放器显示状态
+     */
     togglePlayer() {
-      alert(1);
+      this.setFullScreen(!this.fullScreen);
+    },
+
+    /**
+     * 点击切换暂停或播放
+     */
+    togglePlayState() {
+      this.setPlayState(!this.playState);
+    },
+
+    /**
+     * vuex提供的存数据的语法糖，映射mutations里的方法
+     * @type {String}
+     */
+    ...mapMutations({
+      setFullScreen: 'SET_FULL_SCREEN',
+      setPlayState: 'SET_PLAY_STATE'
+    })
+  },
+  watch: {
+    currentSong(newSong) {
+      setTimeout(() => {
+        this.$refs.audio.play();
+      }, 5000);
+    },
+    playState(newState) {
+      let audio = this.$refs.audio;
+
+      setTimeout(() => {
+        if (newState) {
+          audio.play();
+        } else {
+          audio.pause();
+        }
+      }, 1000);
     }
   }
 };
@@ -94,6 +161,7 @@ export default {
   right: 0;
   z-index: 150;
   background-color: @color-background;
+
   .background {
     position: absolute;
     top: 0;
@@ -108,11 +176,13 @@ export default {
   .top {
     position: relative;
     margin-bottom: 25px;
+
     .icon-back-wrapper {
       position: absolute;
       top: 0;
       left: 6px;
       z-index: 50;
+
       .icon-back {
         display: block;
         padding: 10px;
@@ -121,6 +191,7 @@ export default {
         color: @color-theme;
       }
     }
+
     .song-name {
       width: 70%;
       margin: 0 auto;
@@ -130,6 +201,7 @@ export default {
       font-size: @font-size-large;
       color: @color-text;
     }
+
     .singer-name {
       line-height: 20px;
       text-align: center;
@@ -143,9 +215,20 @@ export default {
     bottom: 170px;
     width: 100%;
     white-space: nowrap;
+
     .cd {
       width: 100%;
       text-align: center;
+
+      // cd旋转动画
+      &.play {
+        animation: rotate 20s linear infinite;
+      }
+
+      &.pause {
+        animation-play-state: paused;
+      }
+
       img {
         width: 80%;
         height: 80%;
@@ -183,8 +266,28 @@ export default {
         .icon-play {
           font-size: 40px;
         }
+        .icon-pause {
+          font-size: 40px;
+        }
       }
     }
+  }
+}
+.normal-enter-active,
+.normal-leave-active {
+  transition: all 0.5s;
+  .top .bottom {
+    transition: all 0.5s cubic-bezier(0.86, 0.18, 0.82, 1.32);
+  }
+}
+.normal-enter,
+.normal-leave-to {
+  opacity: 0;
+  .top {
+    transform: translate3d(0, -100px, 0);
+  }
+  .bottom {
+    transform: translate3d(0, 100px, 0);
   }
 }
 .mini-player {
@@ -204,15 +307,26 @@ export default {
     img {
       width: 100%;
       border-radius: 50%;
+
+      // cd旋转动画
+      &.play {
+        animation: rotate 10s linear infinite;
+      }
+
+      &.pause {
+        animation-play-state: paused;
+      }
     }
   }
   .mini-content-wrapper {
     flex: 1;
     line-height: 20px;
     .mini-song-name {
+      .no-wrap;
       font-size: @font-size-medium;
     }
     .mini-singer-name {
+      .no-wrap;
       font-size: @font-size-small;
       color: @color-text-l;
     }
@@ -222,9 +336,28 @@ export default {
     width: 30px;
     padding: 0 10px;
     color: @color-sub-theme;
-    .icon-play {
+    .icon-play-mini {
       font-size: 30px;
     }
+    .icon-pause-mini {
+      font-size: 30px;
+    }
+  }
+}
+.mini-enter-active,
+.mini-leave-active {
+  transition: all 0.5s;
+}
+.mini-leave-to,
+.mini-enter {
+  opacity: 0;
+}
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
